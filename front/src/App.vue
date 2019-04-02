0c83bc945e3e95659dd00dedbb4578276e527993
Antoine <antoine.maluta@pingflow.fr>
2019-04-02 09:26:54 +0200

    Feat (project-stage) : Removed useless files

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Lorem ipsum
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
